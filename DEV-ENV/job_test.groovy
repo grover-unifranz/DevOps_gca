5e845cb74765cd7cf0e5e9c5bcc3435d3c4a5461
Grover Condori <doc.grover.condori.ac@unifranz.edu.bo>
2026-05-06 01:10:32 -0400

    pipeline 345

---
+++ DEV-ENV/job_test.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent {
+        label 'slave1'
+    }
+    stages {
+        stage("TEST INICIAL") {
+            steps {
+                sh "echo 'HOLA MUNDO'"
+            }
+        }
+        stage("Esperando") {
+            steps {
+                script {
+                    echo 'espera 15 segundos'
+                    sleep(15)
+                }
+            }
+        }
+    }
+}
